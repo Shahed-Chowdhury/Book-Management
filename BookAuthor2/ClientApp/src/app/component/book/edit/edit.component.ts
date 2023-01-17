f0db73102c5fdcce0fafce06f3aa7273ab946d20
Shahed-Chowdhury <cslintern.shahed02@gmail.com>
2023-01-17 18:24:34 +0600

    added edit page

--- BookAuthor2/ClientApp/src/app/component/book/edit/edit.component.ts
+++ BookAuthor2/ClientApp/src/app/component/book/edit/edit.component.ts
@@ -1,6 +1,7 @@
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 import { Component, OnInit } from '@angular/core';
+import { faInfoCircle, faPenNib, faTrash, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-edit',
@@ -13,6 +14,11 @@ export class EditComponent implements OnInit {
   book: any
   authors: any
   bookId!: Number 
+  publishers: any
+  faInfo = faInfoCircle;
+  faPenNib = faPenNib;
+  faCross = faTrash;
+  faPlus = faPlusCircle;
 
   constructor(private apiservice: ApiService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
@@ -21,12 +27,14 @@ export class EditComponent implements OnInit {
       !Number(param.get('id')) ? this.router.navigate(['/books']) : this.bookId = Number(param.get('id'));
     })
     this.getBookDetails(this.bookId)
+    this.getPublishers()
   }
 
   getBookDetails(id: Number)
   {
     this.apiservice.getBookById(id).subscribe(res => {
       this.authors = res
+      //console.log(new Date(this.authors.data.publishedDate.split("T")[0]))
       console.log(this.authors)
     })
   }
@@ -36,6 +44,14 @@ export class EditComponent implements OnInit {
     this.apiservice.getAuthors().subscribe(res => {
       this.authors = res;
       console.log(this.authors)
+    })
+  }
+
+  getPublishers()
+  {
+    this.apiservice.getPublishers().subscribe(res => {
+      this.publishers = res
+      console.log(this.publishers)
     })
   }
 
@@ -58,6 +74,16 @@ export class EditComponent implements OnInit {
       alert("Error")
       console.log(err)
     })
+  }
+
+  authorMouseEnter(event: any)
+  {
+    event.target.style.fontSize = "120%";
+  }
+
+  authorMouseLeave(event: any)
+  {
+    event.target.style.fontSize = "100%";
   }
 
 }
