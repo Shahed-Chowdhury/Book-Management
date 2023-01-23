cc79ec28abb35c178ea81a5c29a4ced0b7a794f1
Shahed-Chowdhury <cslintern.shahed02@gmail.com>
2023-01-23 13:59:11 +0600

    nav links active changed

--- BookAuthor2/ClientApp/src/app/component/navbar/navbar.component.ts
+++ BookAuthor2/ClientApp/src/app/component/navbar/navbar.component.ts
@@ -12,4 +12,19 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  activeNavLink(event: any)
+  {
+    let navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(links => links.classList.remove('active'))
+    event.target.classList.add('active')
+  }
+
+  activeNavBrand()
+  {
+    var navLinks = document.querySelectorAll('.nav-link')
+    navLinks.forEach(links => links.classList.remove('active'))
+    navLinks[0].classList.add('active')
+  }
+
 }
+
